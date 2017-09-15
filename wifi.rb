627d817c89f20ca605eb953ef6cfc62a91416472
Krzysztof Jagiello <me@kjagiello.com>
2017-09-15 12:31:47 +0200

    Version bump

--- wifi.rb
+++ wifi.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Wifi < Formula
   homepage 'https://github.com/kjagiello/wifi'
-  url 'https://github.com/kjagiello/wifi/archive/1.0.0.tar.gz'
-  sha256 '2a674e5258ba3b0bcc1c5406b55cea9ca59777521f4cf86b540bd5b84d1cfe67'
+  url 'https://github.com/kjagiello/wifi/archive/1.0.1.tar.gz'
+  sha256 '4fe091c043661dcc3062e705e197bb3c7160a8b7c7dd1e0c464a37a04afdd04b'
   version '1.0'
 
   def install
